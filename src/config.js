81294df4c582f3af911743226fc4e518615edf19
Anchen <acmhuang@gmail.com>
2019-11-15 15:33:38 -0500

    setting up react to work with aws amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "list-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://lparjfk05h.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Rx6sNrGEK",
+    APP_CLIENT_ID: "3bu730tardbdt3qmjr954njpt",
+    IDENTITY_POOL_ID: "us-east-1:b132f645-c676-4279-ae0d-1074cd87beb7"
+  }
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Link, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import HomePage from "../components/HomePage/HomePage";
 import DashboardPage from "../components/DashboardPage/DashboardPage";
 import CreatePage from "../components/CreatePage/CreatePage";
 import EditPage from "../components/EditPage/EditPage";
+import NotFoundPage from "../components/NotFoundPage/NotFoundPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Link path="/" component={HomePage} exact={true} />
-        <Link path="/dashboard" component={DashboardPage} />
-        <Link path="/create" component={CreatePage} />
-        <Link path="/edit" component={EditPage} />
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/create" component={CreatePage} />
+        <Route path="/edit" component={EditPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
   );
